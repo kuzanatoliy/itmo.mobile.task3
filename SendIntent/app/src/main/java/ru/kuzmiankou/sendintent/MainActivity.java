86e42e28edb43ac53fc85adc3d6bc94e42ae113f
kuzanatoliy <njkznu@gmail.com>
2017-11-10 00:10:09 +0300

    Create project

---
+++ SendIntent/app/src/main/java/ru/kuzmiankou/sendintent/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.kuzmiankou.sendintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "hello, dude!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessage.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
